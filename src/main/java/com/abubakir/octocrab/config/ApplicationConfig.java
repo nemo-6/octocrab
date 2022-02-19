a99e8fe04008700af2da478a9038b41b7dc8fb39
joao.abubakir <abubakirjoaopedro@gmail.com>
2022-02-19 07:08:37 -0300

    Added support for OkHTTP

--- src/main/java/com/abubakir/octocrab/config/ApplicationConfig.java
+++ src/main/java/com/abubakir/octocrab/config/ApplicationConfig.java
@@ -1,7 +1,15 @@
 package com.abubakir.octocrab.config;
 
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public OkHttpClient httpClient() {
+        return new OkHttpClient();
+    }
+
 }
